ecad7cf9af4152810296deda9ad891224695c81e
Melanie Klein <klein@dial.de>
2021-07-26 11:14:27 +0200

    GUI: FileLock.py auf das Nötigste runtergebrochen

--- FileLock.py
+++ FileLock.py
@@ -1,15 +1,3 @@
 from GUI.StartWindow import *
 
-#pw = "Mein PW"
-#data = bytearray("Meine Bytes", encoding="UTF-8")
-#crypt = encrypter()
-
-#crypted = crypt.encrypt(data, pw)
-# print(crypted)
-
-#decrypted = crypt.decrypt(crypted, pw)
-# print(decrypted)
-# print(data)
-
-startwindow = StartWindow()
-startwindow.start()
+StartWindow().start()
